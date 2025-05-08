0a6f456c64ceabcd31894d09e2d264cb522cb091
김상어 <rjsgh1108@naver.com>
2025-05-08 09:36:27 +0900

    Add files via upload

---
+++ SLQ_Consol_Board/SLQ_Console Board v1.0.4/src/com/shark/SLQ_ConsoleBoard/Main.java
@@ -0,0 +1,9 @@
+package com.shark.SLQ_ConsoleBoard;
+
+import com.shark.SLQ_ConsoleBoard.site.SiteMain;
+
+public class Main {
+	public static void main(String[] args) {
+		SiteMain.run();  // 기존 게시판에서 site로 확장
+	}
+}
